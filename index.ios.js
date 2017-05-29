f6b47d6809dfa79c692fbb04cb10793159601ed0
andrew.nelsonrd@gmail.com <andrew.nelsonrd@gmail.com>
2017-05-29 14:45:35 -0700

    first commit

---
+++ index.ios.js
@@ -0,0 +1,8 @@
+/**
+ * R10 App
+ * @flow
+ */
+import { AppRegistry } from 'react-native';
+import R10 from './js'
+
+AppRegistry.registerComponent('R10', () => R10);
---
+++ js/index.js
@@ -0,0 +1,24 @@
+/**
+ * R10 App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Text,
+  View
+} from 'react-native';
+
+export default class R10 extends Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          Welcome to React Native!
+        </Text>
+      </View>
+    );
+  }
+}
+
